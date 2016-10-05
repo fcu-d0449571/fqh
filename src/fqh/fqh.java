addb48cb9146eba6ba714a9315423975d2a93211
Lin Shinhong <Lin Shinhong@DESKTOP-UEFS22M>
2016-10-05 22:49:23 +0800

    功課

---
+++ src/fqh/fqh.java
@@ -0,0 +1,17 @@
+package fqh	;
+
+public class fqh {
+
+ public static void main(String[] args) {
+  
+  
+
+   String lyrics ="Let it go!Let it go! Can't hold it back anymore";
+   
+   lyrics=lyrics.replaceAll("it","her");
+   
+   lyrics=lyrics.replaceAll("Cannot","Can't");
+   System.out.println(lyrics);
+ 
+ }
+ }
